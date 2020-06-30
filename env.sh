20cd58d52fc39eccae2282f8530dfe94896d3e7f
jennliny <jennliny@brandeis.edu>
2020-07-01 01:39:39 +0800

    deleted env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-export clientID="450134845444-t6qjj3n28duaicmsqeveokh8p2gjeahu.apps.googleusercontent.com"
-export clientSecret="yo8MC3Isnrg78dOjWpPhZJrC"
-export callbackURL="http://localhost:5000/login/authorized"
-export MONGODB_URI="mongodb://localhost/authDemo"
