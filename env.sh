22b115c1623e6938db796c6a5bc7261fcf28dae8
jennliny <jennliny@brandeis.edu>
2020-07-01 01:02:56 +0800

    major change to virtual writing workshop

---
+++ env.sh
@@ -0,0 +1,4 @@
+export clientID="450134845444-t6qjj3n28duaicmsqeveokh8p2gjeahu.apps.googleusercontent.com"
+export clientSecret="yo8MC3Isnrg78dOjWpPhZJrC"
+export callbackURL="http://localhost:5000/login/authorized"
+export MONGODB_URI="mongodb://localhost/authDemo"
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const Writing = require('../models/Writing');
+const Comment = require('../models/Comment');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',
+  async (req, res, next) => {
+    try {
+
+      res.locals.posts = await Writing.find({});
+      res.locals.posts.sort((a,b) => b.createdAt - a.createdAt);
+      res.render('index', { title: 'Express' });
+    } catch (e) {
+      next(e)
+    }
+
+});
+
+router.get('/remove/:itemId',
+  //isLoggedIn,
+  async (req, res, next) => {
+      console.log("inside /remove/:itemId")
+      await Writing.remove({_id:req.params.itemId});
+      res.redirect('/')
+});
+
+router.get('/comments/:itemId',
+  //isLoggedIn,
+  async (req, res, next) => {
+      console.log("inside /")
+
+      res.render('comments')
 });
 
 module.exports = router;
